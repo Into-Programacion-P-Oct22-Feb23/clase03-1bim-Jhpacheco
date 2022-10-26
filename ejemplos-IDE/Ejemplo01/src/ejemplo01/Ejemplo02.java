5dc70673e809daa35cc2f5b30c252da6210885a9
jhpacheco <jhpachecor@gmial.com>
2022-10-26 10:49:01 -0500

    ejecutando java

---
+++ ejemplos-IDE/Ejemplo01/src/ejemplo01/Ejemplo02.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejemplo01;
+
+/**
+ *
+ * @author L1B3R4T0
+ */
+public class Ejemplo02 {
+      
+   
+    public static void main(String[] args) {
+        System.out.println("Nueva leyenda");
+    }
+    
+}
